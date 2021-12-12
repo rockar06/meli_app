53b05118d149b1660f556b9c206ed62398afba20
Armando Ruiz <aranruiz13@gmail.com>
2021-12-11 20:37:52 -0600

    Add ProductsDataSource.kt to load data from pagination source

---
+++ app/src/main/java/com/example/meliapp/data/datasource/ProductsDataSource.kt
@@ -0,0 +1,47 @@
+package com.example.meliapp.data.datasource
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.example.meliapp.data.api.MeLiApi
+import com.example.meliapp.data.model.Results
+
+const val NETWORK_PAGE_SIZE = 20
+const val MELI_STARTING_PAGE_INDEX = 0
+
+class ProductsDataSource(
+    private val service: MeLiApi,
+    private val query: String
+) : PagingSource<Int, Results>() {
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Results> {
+        val position = params.key ?: MELI_STARTING_PAGE_INDEX
+        return try {
+            val response = service.searchForProducts(query, position, params.loadSize)
+            val products = response.results
+            val nextKey = if (products.isEmpty()) {
+                null
+            } else {
+                position + params.loadSize
+            }
+            val prevKey = if (position == MELI_STARTING_PAGE_INDEX) {
+                null
+            } else {
+                position - NETWORK_PAGE_SIZE
+            }
+            LoadResult.Page(
+                data = products,
+                prevKey = prevKey,
+                nextKey = nextKey
+            )
+        } catch (exception: Exception) {
+            return LoadResult.Error(exception)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, Results>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            state.closestPageToPosition(anchorPosition)?.prevKey
+                ?: state.closestPageToPosition(anchorPosition)?.nextKey
+        }
+    }
+}
---
+++ app/src/test/java/com/example/meliapp/data/datasource/ProductsDataSourceTest.kt
@@ -0,0 +1,109 @@
+package com.example.meliapp.data.datasource
+
+import androidx.paging.PagingSource
+import com.example.meliapp.data.api.MeLiApi
+import com.example.meliapp.data.model.Paging
+import com.example.meliapp.data.model.Results
+import com.example.meliapp.data.model.SearchResult
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Test
+import org.mockito.kotlin.any
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+import retrofit2.HttpException
+
+@ExperimentalCoroutinesApi
+class ProductsDataSourceTest {
+
+    private val mockMeLiApi: MeLiApi = mock()
+    private val mockQuery = "query"
+    private val mockResults: Results = mock()
+    private val mockedListOfResults = listOf(mockResults, mockResults, mockResults)
+
+    @Test
+    internal fun load_onSuccessfulLoadOfItemKeyedData_returnsPage() = runBlockingTest {
+        mockApiResponse()
+        val pagingSource = ProductsDataSource(mockMeLiApi, mockQuery)
+        assertThat(
+            pagingSource.load(
+                PagingSource.LoadParams.Refresh(
+                    key = null,
+                    loadSize = 60,
+                    placeholdersEnabled = false
+                )
+            )
+        ).isEqualTo(
+            PagingSource.LoadResult.Page(
+                data = mockedListOfResults,
+                prevKey = null,
+                nextKey = 60
+            )
+        )
+    }
+
+    private fun mockApiResponse() = runBlockingTest {
+        whenever(mockMeLiApi.searchForProducts(any(), any(), any())).thenReturn(
+            SearchResult(
+                paging = Paging(100, 100, 0, 60),
+                results = mockedListOfResults
+            )
+        )
+    }
+
+    @Test
+    fun load_onSuccessfulLoadOfItemKeyedData_returnsNullNextKey() = runBlockingTest {
+        mockApiResponseNoMoreResults()
+        val pagingSource = ProductsDataSource(mockMeLiApi, mockQuery)
+        assertThat(
+            pagingSource.load(
+                PagingSource.LoadParams.Refresh(
+                    key = null,
+                    loadSize = 60,
+                    placeholdersEnabled = false
+                )
+            )
+        ).isEqualTo(
+            PagingSource.LoadResult.Page(
+                data = emptyList(),
+                prevKey = null,
+                nextKey = null
+            )
+        )
+    }
+
+    private fun mockApiResponseNoMoreResults() = runBlockingTest {
+        whenever(mockMeLiApi.searchForProducts(any(), any(), any())).thenReturn(
+            SearchResult(
+                paging = Paging(100, 100, 0, 60),
+                results = emptyList()
+            )
+        )
+    }
+
+    @Test
+    fun load_onErrorLoadingItems_returnPageError() = runBlockingTest {
+        mockApiResponseThrowsError()
+        val pagingSource = ProductsDataSource(mockMeLiApi, mockQuery)
+        assertThat(
+            pagingSource.load(
+                PagingSource.LoadParams.Refresh(
+                    key = null,
+                    loadSize = 60,
+                    placeholdersEnabled = false
+                )
+            )
+        ).isInstanceOf(PagingSource.LoadResult.Error::class.java)
+    }
+
+    private fun mockApiResponseThrowsError() = runBlockingTest {
+        whenever(
+            mockMeLiApi.searchForProducts(
+                any(),
+                any(),
+                any()
+            )
+        ).thenThrow(HttpException::class.java)
+    }
+}
