0740efb1c38b67aa303d78680a5db9dc15d9dd3a
Armando Ruiz <aranruiz13@gmail.com>
2021-12-11 18:53:38 -0600

    Add remain providers in NetworkProvides

--- app/src/main/java/com/example/meliapp/di/NetworkProvides.kt
+++ app/src/main/java/com/example/meliapp/di/NetworkProvides.kt
@@ -1,20 +1,45 @@
 package com.example.meliapp.di
 
 import com.example.meliapp.BuildConfig
+import com.example.meliapp.data.repository.MeLiRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.time.Duration
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class NetworkProvides {
 
+    private val requestTimeout = Duration.ofSeconds(30)
+
     @Provides
-    fun retrofitInstance(): Retrofit {
+    @Singleton
+    fun providesRetrofitInstance(okHttpClient: OkHttpClient): Retrofit {
         return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
             .baseUrl(BuildConfig.BASE_URL)
+            .client(okHttpClient)
             .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(requestTimeout)
+            .writeTimeout(requestTimeout)
+            .addInterceptor(NetworkInterceptor.getInterceptor())
+            .build()
+    }
+
+    @Provides
+    fun providesMeliRepositoryInstance(retrofit: Retrofit): MeLiRepository {
+        return retrofit.create(MeLiRepository::class.java)
     }
 }
--- app/src/test/java/com/example/meliapp/di/NetworkProvidesTest.kt
+++ app/src/test/java/com/example/meliapp/di/NetworkProvidesTest.kt
@@ -1,23 +1,47 @@
 package com.example.meliapp.di
 
+import com.example.meliapp.data.repository.MeLiRepository
 import com.google.common.truth.Truth.assertThat
-import org.junit.Before
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.junit.Test
+import org.mockito.kotlin.any
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+import retrofit2.Retrofit
 
 private const val EXPECTED_URL = "https://api.mercadolibre.com/"
 
 class NetworkProvidesTest {
 
-    private lateinit var systemUnderTest: NetworkProvides
+    private val systemUnderTest: NetworkProvides = NetworkProvides()
+    private val mockOkHttpClient: OkHttpClient = mock()
+    private val mockRetrofit: Retrofit = mock()
+    private val mockMeLiRepository: MeLiRepository = mock()
 
-    @Before
-    fun setUp() {
-        systemUnderTest = NetworkProvides()
+    @Test
+    fun providesRetrofitInstance_baseUrl_isUrlExpected() {
+        val retrofitInstance = systemUnderTest.providesRetrofitInstance(mockOkHttpClient)
+        assertThat(retrofitInstance.baseUrl().toString()).isEqualTo(EXPECTED_URL)
     }
 
     @Test
-    fun retrofitInstance_baseUrl_isUrlExpected() {
-        val retrofitInstance = systemUnderTest.retrofitInstance()
-        assertThat(retrofitInstance.baseUrl().toString()).isEqualTo(EXPECTED_URL)
+    fun providesOkHttpClientInstance_getInterceptors_hasInterceptors() {
+        val okHttpClientInstance = systemUnderTest.providesOkHttpClient()
+        assertThat(okHttpClientInstance.interceptors).hasSize(1)
+    }
+
+    @Test
+    fun providesOkHttpClientInstance_getInterceptors_firstIsHttpLogger() {
+        val okHttpClientInstance = systemUnderTest.providesOkHttpClient()
+        val firstInterceptor = okHttpClientInstance.interceptors.first()
+        assertThat(firstInterceptor).isInstanceOf(HttpLoggingInterceptor::class.java)
+    }
+
+    @Test
+    fun providesMeliRepositoryInstance_getInstance_instanceOfMeliRepository() {
+        whenever(mockRetrofit.create(MeLiRepository::class.java)).thenReturn(mockMeLiRepository)
+        val meliRepository = systemUnderTest.providesMeliRepositoryInstance(mockRetrofit)
+        assertThat(meliRepository).isInstanceOf(MeLiRepository::class.java)
     }
 }
