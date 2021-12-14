82cc6726ea21073df7b5717a720d130868585404
Armando Ruiz <aranruiz13@gmail.com>
2021-12-14 10:26:16 -0600

    Add ProductDetailPageFragment.kt to display product details

---
+++ app/src/main/java/com/example/meliapp/view/fragment/ProductDetailPageFragment.kt
@@ -0,0 +1,30 @@
+package com.example.meliapp.view.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.example.meliapp.databinding.FragmentProductDetailBinding
+
+class ProductDetailPageFragment : Fragment() {
+
+    private var _binding: FragmentProductDetailBinding? = null
+    private val binding get() = _binding!!
+    private val arguments: ProductDetailPageFragmentArgs by navArgs()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentProductDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
